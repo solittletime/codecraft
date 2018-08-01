3d2712b81b7f4972abfd674e748bf529f1283357
“solittletime” <solittletime@users.noreply.github.com>
2018-08-01 07:16:52 -0400

    add delete and content projection

--- src/app/joke-form/joke-form.component.ts
+++ src/app/joke-form/joke-form.component.ts
@@ -1,11 +1,14 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 import { Joke } from '../joke';
 
 @Component({
   selector: 'app-joke-form',
   templateUrl: './joke-form.component.html',
-  styleUrls: ['./joke-form.component.css']
+  styleUrls: ['./joke-form.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.Native
+  // encapsulation: ViewEncapsulation.None
 })
 export class JokeFormComponent {
   @Output() jokeCreated = new EventEmitter<Joke>();
